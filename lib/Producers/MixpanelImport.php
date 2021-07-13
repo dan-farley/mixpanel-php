c8abc707bff84972baf5423de31d86210ba52b2c
dan-farley <32692888+dan-farley@users.noreply.github.com>
2021-07-13 15:12:49 -0400

    Add import functionality

---
+++ lib/Producers/MixpanelImport.php
@@ -0,0 +1,44 @@
+<?php
+require_once(dirname(__FILE__) . "/MixpanelBaseProducer.php");
+require_once(dirname(__FILE__) . "/MixpanelPeople.php");
+require_once(dirname(__FILE__) . "/../ConsumerStrategies/CurlConsumer.php");
+
+/**
+ * Provides an API to track past events on Mixpanel
+ */
+class Producers_MixpanelImport extends Producers_MixpanelBaseProducer {
+
+    /**
+     * An array of properties to attach to every tracked event
+     * @var array
+     */
+    private $_super_properties = array("mp_lib" => "php");
+
+
+    /**
+     * Track an event defined by $event associated with metadata defined by $properties
+     * @param string $event
+     * @param array $properties
+     */
+    public function import($event, $properties = array()) {
+
+        // if no token is passed in, use current token
+        if (!array_key_exists("token", $properties)) $properties['token'] = $this->_token;
+
+        // if no time is passed in, use the current time
+        if (!array_key_exists('time', $properties)) $properties['time'] = time();
+
+        $params['event'] = $event;
+        $params['properties'] = array_merge($this->_super_properties, $properties);
+
+        $this->enqueue($params);
+    }
+
+    /**
+     * Returns the "events" endpoint
+     * @return string
+     */
+    function _getEndpoint() {
+        return $this->_options['import_endpoint'];
+    }
+}
